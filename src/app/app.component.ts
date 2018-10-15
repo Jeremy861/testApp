0b03890c6461d1ba1cf40f880d68ab05e0e8068e
Jeremy Jy <jeremyjy@alexiss-Mac.local>
2018-10-15 07:30:37 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { PresentationPage } from '../pages/presentation/presentation';
+import { FormPage } from '../pages/form/form';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +24,9 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Presentation', component: PresentationPage },
+      { title: 'Form', component: FormPage },
     ];
 
   }
